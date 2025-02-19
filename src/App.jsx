0c9b7592b482fd31bdc3d4e402cb59412fd3a447
Harshit Gupta <iamharshit999@gmail.com>
2025-02-20 01:41:19 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import './App.css'
+import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+
+  const { user } = useAuth0();
+  console.log("current user : ",user);
 
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { Auth0Provider } from '@auth0/auth0-react';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+    domain="dev-d14zuex37xe2fbi7.us.auth0.com"
+    clientId="XalrVXC0mu18GZmp0q25ycqvtIRm0Tea"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>,
 )
