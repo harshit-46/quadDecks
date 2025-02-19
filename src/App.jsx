4b3169a1638376e1271afaae5ee0c0f905c02e20
Harshit Gupta <iamharshit999@gmail.com>
2025-02-20 01:58:01 +0530

    login feature enabled

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
 
-  const { user } = useAuth0();
+  const { user , loginWithRedirect , isAuthenticated , logout} = useAuth0();
   console.log("current user : ",user);
 
   return (
     <>
-      <p className='text-red-500'>Hello</p>
+    {isAuthenticated && <h3>Hello , {user.name}</h3>}
+    {
+        isAuthenticated ? <button onClick={ (e) => {logout()}}>Logout</button> : <button onClick={ (e) => loginWithRedirect()}>Login</button>
+    }
     </>
   )
 }
